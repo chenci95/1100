9c4773df9d557f67b188c48032a93f26aa62d447
chenci95 <77560646+chenci95@users.noreply.github.com>
2021-01-25 18:52:32 +0800

    更新

---
+++ data.js
@@ -0,0 +1,177 @@
+[
+{"node": [{
+		"text": "1盘带聊天室",
+		"lei": "pan",
+		"url": "http://p1.demo99.cc",
+		"img": "http://www.maofan.co/bytech/pan/1.png",
+		"name": "dream",
+		"pass": "123456",
+		"price": "5000"
+	},{
+		"text": "2盘带聊天室",
+		"lei": "pan",
+		"url": "http://p2.demo99.cc",
+		"img": "http://www.maofan.co/bytech/pan/2.png",
+		"name": "dream",
+		"pass": "123456",
+		"price": "5000"
+	},{
+		"text": "3盘带聊天室",
+		"lei": "pan",
+		"url": "http://p3.demo99.cc",
+		"img": "http://www.maofan.co/bytech/pan/3.png",
+		"name": "dream",
+		"pass": "123456",
+		"price": "5000"
+	},{
+		"text": "4盘带聊天室",
+		"lei": "pan",
+		"url": "http://p4.demo99.cc",
+		"img": "http://www.maofan.co/bytech/pan/4.png",
+		"name": "dream",
+		"pass": "123456",
+		"price": "5000"
+	},{
+		"text": "5盘带聊天室",
+		"lei": "pan",
+		"url": "http://p5.demo99.cc",
+		"img": "http://www.maofan.co/bytech/pan/5.png",
+		"name": "dream",
+		"pass": "123456",
+		"price": "5000"
+	},{
+		"text": "6盘带聊天室",
+		"lei": "pan",
+		"url": "http://p6.demo99.cc",
+		"img": "http://www.maofan.co/bytech/pan/6.png",
+		"name": "dream",
+		"pass": "123456",
+		"price": "5000"
+	},{
+		"text": "7盘带聊天室",
+		"lei": "pan",
+		"url": "http://p7.demo99.cc",
+		"img": "http://www.maofan.co/bytech/pan/7.png",
+		"name": "dream",
+		"pass": "123456",
+		"price": "5000"
+	},{
+		"text": "2",
+		"lei": "chuan",
+		"url": "https://www.baidu.com/",
+		"img": "http://www.maofan.co/1100/allimg/pank/007.png",
+		"name": "dream",
+		"pass": "123456",
+		"price": "5000"
+	},{
+		"text": "3",
+		"lei": "wei",
+		"url": "https://www.baidu.com/",
+		"img": "https://img.mituo.cn/upload/201808/1535420577.jpg",
+		"name": "dream",
+		"pass": "123456",
+		"price": "5000"
+	}
+	]
+},
+{"fuwuqi": [{
+		"text": "香港沙田 A型",
+		"price": "1000",
+		"cpu": "E3-1220(4核4线程)",
+		"memory": "4G",
+		"broadband": "5M",
+		"ddos": "5G",
+		"disk": "SSD 120G"
+	},{
+		"text": "香港沙田 B型",
+		"price": "1300",
+		"cpu": "E3-1230(4核8线程)",
+		"memory": "8G",
+		"broadband": "5M",
+		"ddos": "5G",
+		"disk": "SSD 120G"
+	},{
+		"text": "香港沙田 C型",
+		"price": "1900",
+		"cpu": "E3-1230(4核8线程)",
+		"memory": "8G",
+		"broadband": "10M",
+		"ddos": "5G",
+		"disk": "SSD 240G"
+	},{
+		"text": "香港私人 A型",
+		"price": "1200",
+		"cpu": "E3-1230(4核8线程)",
+		"memory": "8G",
+		"broadband": "10M",
+		"ddos": "5G",
+		"disk": "SATA 1T"
+	},{
+		"text": "香港私人 B型",
+		"price": "1900",
+		"cpu": "E5-2650L(8核16线程)",
+		"memory": "16G",
+		"broadband": "20M",
+		"ddos": "5G",
+		"disk": "SATA 1T"
+	},{
+		"text": "香港私人 C型",
+		"price": "2100",
+		"cpu": "E5-2650L(8核16线程)",
+		"memory": "32G",
+		"broadband": "20M",
+		"ddos": "5G",
+		"disk": "SATA 1T"
+	},{
+		"text": "香港CN2 A型",
+		"price": "1600",
+		"cpu": "L5520(4核8线程)",
+		"memory": "16G",
+		"broadband": "20M",
+		"ddos": "10G",
+		"disk": "SATA 1T"
+	},{
+		"text": "香港CN2 B型",
+		"price": "1900",
+		"cpu": "E3-1230(4核8线程)",
+		"memory": "16G",
+		"broadband": "20M",
+		"ddos": "10G",
+		"disk": "SATA 1T"
+	},{
+		"text": "香港CN2 C型",
+		"price": "2200",
+		"cpu": "E5-2630l(6核12线程)",
+		"memory": "16G",
+		"broadband": "20M",
+		"ddos": "10G",
+		"disk": "SATA 1T"
+	},{
+		"text": "日韩CN2 A型",
+		"price": "1900",
+		"cpu": "E5-2403(4核4线程)",
+		"memory": "8G",
+		"broadband": "10M",
+		"ddos": "5G",
+		"disk": "SSD 120G"
+	},{
+		"text": "日韩CN2 B型",
+		"price": "1900",
+		"cpu": "E3-1240(4核8线程)",
+		"memory": "16G",
+		"broadband": "20M",
+		"ddos": "10G",
+		"disk": "SATA 1T"
+	},{
+		"text": "日韩CN2 C型",
+		"price": "2300",
+		"cpu": "E5-2650L(10核20线程)",
+		"memory": "16G",
+		"broadband": "20M",
+		"ddos": "10G",
+		"disk": "SATA 1T"
+	}
+	]
+}
+
+]
